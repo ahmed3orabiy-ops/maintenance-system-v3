57ee8cde3d6655401a5f17bbe014555c1a1a2573
ahmed3orabiy-ops <ahmed.3orabiy@gmail.com>
2026-08-03 04:55:16 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyCE5GZAJOaHdSIFC9udxENcHe_EMjjYqV0",
   authDomain: "el-rabeh.firebaseapp.com",
@@ -12,3 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
